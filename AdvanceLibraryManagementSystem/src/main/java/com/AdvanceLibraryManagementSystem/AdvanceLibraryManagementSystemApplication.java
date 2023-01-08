26c5b0b5e7a16571b5267be05b74957cae0013a6
sneha <computer.project2023@gmail.comgit config --global user.name computerproj>
2023-01-08 11:17:56 +0530

    commit structure of the project

---
+++ AdvanceLibraryManagementSystem/src/main/java/com/AdvanceLibraryManagementSystem/AdvanceLibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.AdvanceLibraryManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceLibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceLibraryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ AdvanceLibraryManagementSystem/src/test/java/com/AdvanceLibraryManagementSystem/AdvanceLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdvanceLibraryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceLibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
